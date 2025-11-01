f9115708c239b2cd41b57440f38c0a8ade7fca15
noor fatima <kaspi0965@gmail.com>
2025-11-01 19:57:52 +0500

    PYTHON

--- Python_Basics/02_print_Indentation/main.py
+++ Python_Basics/02_print_Indentation/main.py
@@ -1,5 +1,20 @@
-# Day 2: Exploring print() and indentation in Python
+# Day 2: Exploring print() and comments in Python
 
-print("Welcome to Day 3 of my Python learning journey!")
+print("Welcome to Day 2 of my Python learning journey!")
 
+# print() is a built-in Python function that instructs the computer to display text on the screen.
 
+# Showing different data types
+print("String Example:", "Python")
+print("Integer Example:", 25)
+print("Float Example:", 3.14)
+print("Boolean Example:", True)
+
+# this is a single line comment !
+'''
+this is multi line comment !
+
+Anything after a # symbol is a comment. 
+Python ignores comments, but they are useful for explaining code to human readers.
+
+'''
